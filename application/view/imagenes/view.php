2104b11152177e3e512fad77d9e6caec192b87b8
cristophernic <cristophernic@live.cl>
2018-02-13 19:32:18 -0300

    error

--- application/view/imagenes/view.php
+++ application/view/imagenes/view.php
@@ -3,6 +3,7 @@
       <meta charset='utf-8'>
       <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
       <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'>
+      <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
    </head>
    <body>
       <div class='container'>
