d4b8b45ca35ca51350b5511d59ebea2f5c4cb43f
cristophernic <cristophernic@live.cl>
2018-02-13 19:49:49 -0300

    prueba

--- application/view/imagenes/view.php
+++ application/view/imagenes/view.php
@@ -35,15 +35,16 @@
                         if (contadorIMG == 1){
                             gString = gString + "<div class='row mb-4'>";
                         }
-                        gString = gString + "<div class='col-4'><img alt='200x200' style='width: 300px; height: 300px;' src='<?php echo Config::get('URL'); ?>data/<?php echo $this->user_id; ?>/" + item +"'></div>";
                         
                         if (totJPG <= 4){
+                            gString = gString + "<div class='col-4'><img alt='200x200' style='width: 450px; height: 450px;' src='<?php echo Config::get('URL'); ?>data/<?php echo $this->user_id; ?>/" + item +"'></div>";
                             if (contadorIMG == 2){
                                 gString = gString + "</div>";
                                 contadorIMG = 0;
                             }
                         }
                         else if (totJPG => 6){
+                            gString = gString + "<div class='col-4'><img alt='200x200' style='width: 300px; height: 300px;' src='<?php echo Config::get('URL'); ?>data/<?php echo $this->user_id; ?>/" + item +"'></div>";
                             if (contadorIMG == 3){
                                 gString = gString + "</div>";
                                 contadorIMG = 0;
