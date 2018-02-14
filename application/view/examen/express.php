dee8c19c387dcd89a51f79ab98f900a04270090f
cristopher <cristophernic@live.cl>
2018-02-14 19:45:55 -0300

    push

--- application/view/examen/express.php
+++ application/view/examen/express.php
@@ -2279,6 +2279,7 @@
     var serverURL = "<?php echo Config::get('URL'); ?>";
 
     $("#InicioLink").attr("href", "http://servidor.crecimientofetal.cl/examen/express");
+    $("#InicioLink").html("Volver");
 </script>
 <script src="<?php echo Config::get('URL'); ?>js/static/app.class.js"></script>
 <script src="<?php echo Config::get('URL'); ?>js/static/bootstrap-datepicker.js"></script>
