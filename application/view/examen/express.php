bc9bbdefb7d3eca78b43deae21bb125c23d2f162
cristophernic <cristophernic@live.cl>
2018-03-13 15:51:47 -0300

    mover fur

--- application/view/examen/express.php
+++ application/view/examen/express.php
@@ -46,6 +46,53 @@
                                         <input class="form-control gris-claro" type="text" id="apellido-paciente">
                                     </div>
                                 </div>
+                                <div class="form-group row">
+                                <div class="col">
+                                    <label for="fum2">Fecha de última mestruación</label>
+                                </div>
+                                <div class="col">
+                                    <input class="form-control" type="text" data-date-format="dd/mm/yyyy" data-weekStart="1" onfocus="blur();" name="fum" id="fum-dos">
+                                </div>
+                                <div class="col">
+                                    <label for="dbp" class="col-form-label">Edad Gestacional</label>
+                                </div>
+                                <div class="col">
+                                    <div class="row">
+                       <div class="col">
+                                    <select id="semanasEcoGen" class="form-control">
+                                    <option value="4">4</option>
+<option value="5">5</option>
+                                        <option value="6">6</option>                                        
+<option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                        <option value="11">11</option>
+                                        <option value="12">12</option>
+                                        <option value="13">13</option>
+                                         <option value="14">14</option>
+                                         <option value="15">15</option>
+                                         <option value="16">16</option>
+                                         <option value="17">17</option>
+                                         <option value="18">18</option>
+                                         <option value="19">19</option>
+                                        <option value="20">20</option>
+                                     <option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option><option value="32">32</option><option value="33">33</option><option value="34">34</option><option value="35">35</option><option value="36">36</option><option value="37">37</option><option value="38">38</option><option value="39">39</option><option value="40">40</option><option value="41">41</option><option value="42">42</option></select>
+                        </div>
+                        <div class="col">
+                            <select id="diasEcoGen" class="form-control">
+                                         <option value="0">0</option>
+                                         <option value="1">1</option>
+                                         <option value="2">2</option>
+                                         <option value="3">3</option>
+                                         <option value="4">4</option>
+                                         <option value="5">5</option>
+                                        <option value="6">6</option>
+                                     </select>
+                        </div>
+                    </div>
+                                </div>
+                            </div>
 				<div class="row mt-2 mb-2">
                                     <div class="col-2">
                                         <label class="col-form-label">Edad materna</label>
@@ -167,53 +214,7 @@
                     <div class="card mb-3">
                         <div class="card-body">
                             <h5 class="card-title text-primary">2.- Cálculo de edad gestacional y datos de exámen</h5>
-                            <div class="form-group row">
-                                <div class="col">
-                                    <label for="fum2">Fecha de última mestruación</label>
-                                </div>
-                                <div class="col">
-                                    <input class="form-control" type="text" data-date-format="dd/mm/yyyy" data-weekStart="1" onfocus="blur();" name="fum" id="fum-dos">
-                                </div>
-                                <div class="col">
-                                    <label for="dbp" class="col-form-label">Edad Gestacional</label>
-                                </div>
-                                <div class="col">
-                                    <div class="row">
-                       <div class="col">
-                                    <select id="semanasEcoGen" class="form-control">
-                                    <option value="4">4</option>
-<option value="5">5</option>
-                                        <option value="6">6</option>                                        
-<option value="7">7</option>
-                                        <option value="8">8</option>
-                                        <option value="9">9</option>
-                                        <option value="10">10</option>
-                                        <option value="11">11</option>
-                                        <option value="12">12</option>
-                                        <option value="13">13</option>
-                                         <option value="14">14</option>
-                                         <option value="15">15</option>
-                                         <option value="16">16</option>
-                                         <option value="17">17</option>
-                                         <option value="18">18</option>
-                                         <option value="19">19</option>
-                                        <option value="20">20</option>
-                                     <option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option><option value="32">32</option><option value="33">33</option><option value="34">34</option><option value="35">35</option><option value="36">36</option><option value="37">37</option><option value="38">38</option><option value="39">39</option><option value="40">40</option><option value="41">41</option><option value="42">42</option></select>
-                        </div>
-                        <div class="col">
-                            <select id="diasEcoGen" class="form-control">
-                                         <option value="0">0</option>
-                                         <option value="1">1</option>
-                                         <option value="2">2</option>
-                                         <option value="3">3</option>
-                                         <option value="4">4</option>
-                                         <option value="5">5</option>
-                                        <option value="6">6</option>
-                                     </select>
-                        </div>
-                    </div>
-                                </div>
-                            </div>
+                            
                             <div class="form-group row">
                                 <div class="col">
                                     <label for="dbp" class="col-form-label">Fecha probable de parto</label>
