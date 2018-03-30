77943ea49ba9fdc5d364f0fa814b642a8bcdc0f8
cristophernic <cristophernic@users.noreply.github.com>
2018-03-30 00:31:01 -0300

    Update index.php

--- application/view/pdf/index.php
+++ application/view/pdf/index.php
@@ -78,10 +78,10 @@ $html = '<h1 style="text-align:center">Impresión de Imágenes Gineco-Obstétric
 $pdf->writeHTMLCell(0, 10, '', '', $html, 0, 1, 0, true, '', true);
 
 // Image example with resizing
-$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->images[0], '', '', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->images[1], '110', '', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->images[2], PDF_MARGIN_LEFT, '150', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->images[3], '110', '', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[0], '', '', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[1], '110', '', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[2], PDF_MARGIN_LEFT, '150', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[3], '110', '', 86, 86, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
