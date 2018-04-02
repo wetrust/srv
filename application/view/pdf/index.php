743dc79c796b7377ff33cdc86a20ce55404e5561
cristophernic <cristophernic@live.cl>
2018-04-02 16:54:32 -0300

    definitivo

--- application/view/pdf/index.php
+++ application/view/pdf/index.php
@@ -83,7 +83,7 @@ if (count($this->user_images) == 1){
 }
 else if (count($this->user_images) == 2){
 $pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[0], '60', '', 100, 88, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[1], '60', 160, 100, 88, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+$pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[1], '60', 170, 100, 88, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
 }
 else if (count($this->user_images) == 4){
 $pdf->Image(Config::get('DICOM_DIRECTORY') . $this->user_images[0], '', '', 86, 62, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
