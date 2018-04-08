8824082960d41a8104c554bac27cb03cdabacce5
cristophernic <cristophernic@live.cl>
2018-04-08 20:00:19 -0300

    try

--- application/view/examen/express.php
+++ application/view/examen/express.php
@@ -31,8 +31,8 @@
                 </div>
                 <div class="form-group row mb-0">
                     <div class="col-12">
-                        <div class="form-check">
-                            <label class="ml-0 form-check-label" for="buscar.pacientes.last.view">Ver últimos pacientes</label>
+                        <div class="form-check ml-0">
+                            <label class="form-check-label" for="buscar.pacientes.last.view">Ver últimos pacientes</label>
                             <input type="checkbox" class="form-check-input ml-2" id="buscar.pacientes.last.view">
                             <label class="form-check-label ml-4" id="buscar.pacientes.last.view.text">No</label>
                         </div>
