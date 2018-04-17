17d9225c2f3fb8de526479b1d7bc2e7a1eb5d092
cristophernic <cristophernic@live.cl>
2018-04-17 15:18:00 -0300

    kp8

--- application/model/NombreModel.php
+++ application/model/NombreModel.php
@@ -142,7 +142,7 @@ class NombreModel
             $lastname = $lastname[0] . $lastname[1] . "^" .$lastname[2];
         }
         else if (count($lastname) == 4){
-            $lastname = $lastname[0] . $lastname[1] . "^" .$lastname[2] $lastname[3];
+            $lastname = $lastname[0] . $lastname[1] . "^" .$lastname[2] .$lastname[3];
         }
 
         $sql = "SELECT * FROM DICOMPatients where PatientNam LIKE ? LIMIT 1";
