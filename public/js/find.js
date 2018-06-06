4185e6a54cda33bbe229606fb59b55a5832dd842
Cristopher Castro <cristophernic@live.cl>
2018-06-05 23:24:29 -0400

    git push

--- public/js/find.js
+++ public/js/find.js
@@ -336,7 +336,7 @@ function getExmPrimTrim(){
 
     $.post(serverURL + "examen/get/", data).done(function(response) {
         if (Object.keys(response).length > 0 ){
-            ("#table\\.ecografia\\.primtrim").empty();
+            $("#table\\.ecografia\\.primtrim").empty();
             $.each(response.data, function(i,val){
                 let fila = "<tr><th scope='row'>"+ val.n_examen +"</th><td>" + val.embrion +"</td><td>"+ val.prom_saco+"</td>";
                 $("#table\\.ecografia\\.primtrim").append(fila);
