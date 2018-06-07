f298167d4819fe0828a20fab3352ba8850b49c3d
Cristopher Castro <cristophernic@live.cl>
2018-06-07 10:43:17 -0400

    .

--- application/view/examen/express.php
+++ application/view/examen/express.php
@@ -934,7 +934,7 @@
             </div>
         </div>
         <div class="container" id="ecoObsPrimTrim" style="display:none;">
-            <div class="row bienvenida">
+            <div class="row">
                 <div class="col pr-0">
                     <img class="d-flex ml-3" src="<?php echo Config::get('URL'); ?>img/ic_launcher-web.png" alt="logo" style="max-width: 128px;">
                 </div>
@@ -942,19 +942,6 @@
                     <h6 class="text-secondarymt-2">Ecografía Obstétrica de 1° Trimestre</h6>
                     <h4 class="mb-0">Juana Peres Peres, 12345678-9</h4>
                     <h6 class="text-info">FUM: 12-12-12, EG: 22,2 sem., FPP:12-12-12</h6>
-                </div>
-            <div class=" mb-3">
-                <div class="container">
-                    <div class="media">
-                        
-                        <div class="media-body mt-4">
-                            <h4 class="mt-0 mb-1"><em>Ecografía Obstétrica de 1° Trimestre</em></h4>
-                            <p><em>Ultrasonografía obstétrica básica para profesionales</em></p>
-                        </div>
-                        <div class="media-body mt-4">
-                            <p class="float-right" name="fechaHora" style="color: #f0df90;"></p>
-                        </div>
-                    </div>
                 </div>
             </div>
 <nav aria-label="breadcrumb">
