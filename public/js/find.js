3fcda69bc13017f786947737d3a4b8e4793ca4af
Cristopher Castro <cristophernic@live.cl>
2018-06-08 12:10:30 -0400

    kp8

--- public/js/find.js
+++ public/js/find.js
@@ -48,12 +48,12 @@ $(document).ready(function(){
         }
     });
 
-    $("boton\\.volver\\.step\\.two\\.find").on("click", function(){
+    $("#boton\\.volver\\.step\\.two\\.find").on("click", function(){
         $("#step-one").removeClass("d-none");
         $("#step-two-find").addClass("d-none");
     });
 
-    $("boton\\.volver\\.step\\.three").on("click", function(){
+    $("#boton\\.volver\\.step\\.three").on("click", function(){
         $("#step-one").removeClass("d-none");
         $("#step-three").addClass("d-none");
     });
