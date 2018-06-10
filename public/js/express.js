9a0130af308cbca7561b630ff2e216e9613ed272
Cristopher Castro <cristophernic@live.cl>
2018-06-09 21:58:56 -0400

    .

--- public/js/express.js
+++ public/js/express.js
@@ -223,7 +223,7 @@ $(document).ready(function(){
             if ( Object.keys(response).length > 0 ){
                 $("#table\\.ecografia\\.primtrim").empty();
                 $.each(response.data, function(i,val){
-                    let fila = "<tr><th scope='row'>"+ val.n_examen +"</th><td>" + val.embrion +"</td><td>"+ val.prom_saco+"</td>";
+                    let fila = "<tr><th scope='row'>"+ val.n_examen +"</th><td>" + val.embrion +"</td><td>" + val.embrion +"</td><td>"+ val.prom_saco+"</td>";
                     $("#table\\.ecografia\\.primtrim").append(fila);
                 });
             }
