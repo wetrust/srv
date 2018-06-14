6278e5d2928c9c883969d17dbc2bc3c4f598adce
Cristopher Castro <cristophernic@live.cl>
2018-06-14 17:03:24 -0400

    fotos

--- public/js/express_extras.js
+++ public/js/express_extras.js
@@ -691,4 +691,12 @@ function appLoadBasic(){
             $("#lugar\\.control\\.prenatal").append('<option value="'+ des.hospital_id + '">'+ des.hospital_name  +'</option>');
         });
     });
+
+    $.get( serverURL + "configuracion/getemails", function( data ) {
+        $("#paciente\\.correo\\.config").empty();
+        $.each(data, function (key, des) {
+            let strSelect = "<option value='" + des.email_text +"'>" + des.email_text + "</option>";
+            $("#paciente\\.correo\\.config").append(strSelect);
+        });
+    });
 }
