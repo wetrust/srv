f0ecd4367dcdacd0d1f5a5d5e1ef83f67b6c0d1f
Cristopher Castro <cristophernic@live.cl>
2018-06-15 17:21:27 -0400

    .

--- public/js/express_extras.js
+++ public/js/express_extras.js
@@ -46,7 +46,7 @@ function onHashChange(){
         let RUTPACIENTE = $("#id-paciente").val();
         let FechaExm = $("#input\\.paciente\\.fe").val();
         let dia = FechaExm.substring(0,2);
-        let mes = FechaExm.substring(3,5);
+        let mes = Number(FechaExm.substring(3,5)) -1;
         let ano = FechaExm.substring(6,10);
         FechaExm = new Date(ano, mes, dia).getTime() / 1000
         getDCM(RUTPACIENTE, FechaExm);
