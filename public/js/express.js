ea12a09d063ae43736045b8bd1f9f0888ba40147
Cristopher Castro <cristophernic@live.cl>
2018-06-25 15:29:01 -0400

    .

--- public/js/express.js
+++ public/js/express.js
@@ -1155,7 +1155,7 @@ $(document).ready(function(){
             if ( Object.keys(response).length > 0 ){
                 $("#table\\.ecografia\\.segundotrim").empty();
                 $.each(response.data, function(i,val){
-                    let fila = '<tr><th scope="row" data-id="' + val.eg_examen + '" data-paciente="' + $("#id-paciente").val() +'" data-tipo="1">'+ val.n_examen +'</th><td>' + val.fecha_examen + '</td><td>'+ val.eg_examen +'</td><td>' + val.embrion +'</td><td>'+ val.prom_saco+'</td>';
+                    let fila = '<tr><th scope="row" >'+ val.n_examen +'</th><td>' + val.fecha_examen + '</td><td>'+ val.eg_examen +'</td><td>' + val.pfe_examen +'</td><td>'+ val.pctpeso_examen+'</td><td>' + val.ccca_examen +'</td><td>' + val.pctca_examen +'</td><td>' + val.pctbvm_examen + '<td>';
                     $("#table\\.ecografia\\.segundotrim").append(fila);
                 });
                 $("#table\\.ecografia\\.segundotrim tr").on('click',function(){
