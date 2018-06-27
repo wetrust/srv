9796cf0765512111324eaf15a9fa915bbef8286c
Cristopher Castro <cristophernic@live.cl>
2018-06-26 20:36:12 -0400

    .

--- public/js/express_extras.js
+++ public/js/express_extras.js
@@ -273,9 +273,9 @@ function obtenerEcoSegundo(){
         tipo: 2
     }
 
-   $.post(serverURL + "examen/get", data).done(function (response) {
+    $.post(serverURL + "examen/get", data).done(function (response) {
+        $("#table\\.ecografia\\.segundotrim").empty();
         if ( Object.keys(response).length > 0 ){
-            $("#table\\.ecografia\\.segundotrim").empty();
             $.each(response.data, function(i,val){
                 let fila = '<tr><th scope="row" data-id="' + val.eg_examen + '" data-paciente="' + $("#id-paciente").val() +'" data-tipo="1">'+ val.n_examen +'</th><td>' + val.fecha_examen +'</td><td>' + val.eg_examen +'</td><td>' + val.pfe_examen +'</td><td>'+ val.pctpeso_examen+'</td><td>' + val.ccca_examen +'</td><td>' + val.pctca_examen +'</td><td>' + val.pctbvm_examen + '<td>';
                 $("#table\\.ecografia\\.segundotrim").append(fila);
@@ -284,7 +284,7 @@ function obtenerEcoSegundo(){
                 activateTr(this);
             });
         }
-    })
+    });
 }
 
 function obtenerEcoDoppler(){
