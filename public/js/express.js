9131e3b6e8c8595c4db0f00257ebfceea81cdf30
Cristopher Castro <cristophernic@live.cl>
2018-06-27 00:05:26 -0400

    .

--- public/js/express.js
+++ public/js/express.js
@@ -2756,7 +2756,7 @@ $(document).ready(function(){
                 
                 let data = {
                     id: $("#id-paciente").val(),
-                    tipo: 2,
+                    tipo: 3,
                     data: JSON.stringify(examen)
                 }
 
@@ -2764,7 +2764,7 @@ $(document).ready(function(){
                     $("#table\\.ecografia\\.doppler").empty();
                     if ( Object.keys(response).length > 0 ){
                         $.each(response.data, function(i,val){
-                            let fila = '<tr><th scope="row" data-id="' + val.eg_examen + '">'+ val.n_examen +'</th><td>' + val.fecha_examen + '</td><td>'+ val.eg_examen +'</td><td>' + val.pfe_examen +'</td><td>'+ val.pctpeso_examen+'</td><td>' + val.ccca_examen +'</td><td>' + val.pctca_examen +'</td><td>' + val.pctbvm_examen + '<td>';
+                            let fila = '<tr><th scope="row" data-id="' + val.eg_examen + '" data-id="' + val.eg_examen + '">'+ val.n_examen +'</th><td>' + val.fecha_examen + '</td><td>'+ val.eg_examen +'</td><td>' + val.pfe_examen +'</td><td>'+ val.pctpeso_examen+'</td><td>' + val.ccca_examen +'</td><td>' + val.pctca_examen +'</td><td>' + val.pctbvm_examen + '<td>';
                             $("#table\\.ecografia\\.doppler").append(fila);
                         });
                         $("#table\\.ecografia\\.doppler tr").on('click',function(){
