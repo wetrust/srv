0f39ed14ae0aec1b15d24f078049b218aaecf5fd
Cristopher Castro <cristophernic@live.cl>
2018-07-04 18:47:51 -0400

    keyup

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -832,6 +832,19 @@
                     }
                 });
 
+                $("#datos\\.neonatal\\.talla").on("keyup", function(){
+                    var max = parseInt($(this).attr('max'));
+                    var min = parseInt($(this).attr('min'));
+                    
+                    if ($(this).val() > max){
+                        $(this).val(max);
+                    }
+                    else if ($(this).val() < min)
+                    {
+                        $(this).val(min);
+                    }
+                });
+
                 $("#datos\\.neonatal\\.talla").on("change", function(){
                     var max = parseInt($(this).attr('max'));
                     var min = parseInt($(this).attr('min'));
