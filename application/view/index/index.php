a5dbac0907ad5b6fda97a1f1a89df779266da1ad
Cristopher Castro <cristophernic@live.cl>
2018-08-01 17:49:12 -0400

    .

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -17,19 +17,37 @@
 <body class="h-100 w-100">
   <div class="h-100 w-100" id="main">
     <div class="container">
-      <div class="row pt-3">
+      <div class="row py-3">
         <div class="col mt-5">
           <h1>crecimientofetal.cl</h1>
           <h3 class="mb-3"><small>Ultrasonografía obstétrica básica para profesionales</small></h3>
           <p class="text-justify"><small>Plataforma web diseñado para profesionales de la salud (médicos) vinculados con la vigilancia del control prenatal.<br>El objetivo de esta aplicación es facilitar la valoración de datos obstétricos ecográficos relacionados con la vigilancia del crecimiento fetal. En el ámbito postnatal se valora el crecimiento intrauterino.<br><br>Software obstétrico propiedad intelectual Dr. Rudecindo Lagos, Maternidad Hospital Regional de Temuco, Facultad de Medicina Universidad de La Frontera Temuco - Chile</small></p>
           <div class="btn-group-vertical">
             <button type="button" class="btn btn-raised btn-primary">Iniciar exámen ecográfico</button>
-            <button type="button" class="btn btn-raised btn-primary">Más información</button>
+            <button type="button" class="btn btn-raised btn-default">Más información</button>
           </div>
         </div>
         <div class="col-7 mt-5">
           <img class="ml-lg-5 img-fluid" src="img/feto-computador.png" alt="logo" >
         </div>
+      </div>
+    </div>
+  </div>
+  <div class="h-100 w-100">
+    <div class="container">
+      <h4>Funciones</h4>
+      <div class="row">
+        <div class="col">
+          <p class="text-justify">Sitio web diseñado para profesionales de la salud (médicos) vinculados con la vigilancia del control prenatal.</p>
+          <p class="text-justify">El objetivo de esta aplicación es facilitar la valoración de datos obstétricos ecográficos relacionados con la vigilancia del crecimiento fetal. Actualmente activadas herramientas informaticas para:</p>
+          <ul>
+            <li>Determinación ecográfica de la edad gestacional</li>
+            <li>Valoración de biometrías y crecimiento fetal</li>
+            <li>Flujometria Doppler Materno / Fetal básico</li>
+          </ul>
+          <p class="text-justify">Tradicionalmente se valora crecimiento intrauterino mediante estándar (curvas o tablas) de peso propias de cada región (curva local generalizada). Sin embargo, para una población heterogénea, el parámetro ideal para valorar crecimiento intrauterino no necesariamente es una curva local generalizada, sino curva general individualizada, estándar ajustado a características materno-fetales individuales y, por tanto, trasciende límites geográficos.</p>
+        </div>
+        <div class="col"></div>
       </div>
     </div>
   </div>
