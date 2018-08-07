d5ae63b6e8f662206ed6836f58305c17db622a52
Cristopher Castro <cristophernic@live.cl>
2018-08-07 17:22:50 -0400

    .

--- application/view/index/acerca.php
+++ application/view/index/acerca.php
@@ -34,7 +34,7 @@
                     <img class="d-block mx-auto img-fluid" src="img/Dr_Lagos_copia.jpg" alt="logo" >
                 </div>
                 <div class="col">
-                    <p><small>Dr. Rudecindo Lagos Sandoval</small></p>
+                    <p><strong>Dr. Rudecindo Lagos Sandoval</strong></p>
                     <p class="text-justify"><small>Médico cirujano, especialista en Ginecología y Obstetricia. Docente de pre y postgrado, profesor adscrito al Departamento de Obstetricia y Ginecología de la Facultad de Medicina, Universidad de La Frontera, y al Centro de Excelencia CIGES.  Encargado de la Unidad de Ultrasonografía, Hospital Hernán Hernríquez Aravena de Temuco.<br><br>Línea de interés, valoración del crecimiento fetal, evaluación clínica, ultrasonográfica y flujometría Doopler materno fetal.</small></p>
                 </div>
             </div>
