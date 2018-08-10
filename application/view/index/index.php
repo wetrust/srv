8729e99e93d83b15774a34de8ecde140afbe4907
Cristopher Castro <cristophernic@live.cl>
2018-08-10 16:47:03 -0400

    .

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -58,12 +58,20 @@
       </div>
     </div>
   </div>
-  <div class="h-100 w-100 bg-success generico">
+  <div class="h-100 w-100 bg-primary generico">
     <div class="container">
       <div class="row">
         <div class="col pt-3">
           <p class="text-center text-white">Los cálculos y resultados de los contenidos en esta aplicación web deben ser confirmados antes de ser usados clínicamente, los resultados obtenidos mediante ella, no sustituyen el buen juicio clínico.</p>
-          <p class="text-center text-white">La interpretación de los datos extraídos mediante esta herramienta informática es responsabilidad exclusiva de quien realiza y certifica el examen</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="h-100 w-100 generico">
+    <div class="container">
+      <div class="row">
+        <div class="col pt-3">
+          <p class="text-center">La interpretación de los datos extraídos mediante esta herramienta informática es responsabilidad exclusiva de quien realiza y certifica el examen</p>
         </div>
       </div>
     </div>
