5310f2bfc185f35d42a8940a2a74b4f79a9104ad
Cristopher Castro <cristophernic@live.cl>
2018-08-10 16:43:26 -0400

    .

--- application/view/index/acerca.php
+++ application/view/index/acerca.php
@@ -84,7 +84,7 @@
     </div>
   </div>
   <div class="h-100 w-100 generico" id="aplicaciones">
-    <div class="container py-5">
+    <div class="container py-3">
       <h2 class="text-center"><small>Además puede descargar aplicaciones para Smartphone, creadas para facilitar la evaluación clínico/ecográfica del control prenatal</small></h2>
       <div class="row">
         <div class="col">
