afe17995194124bf55dc4a357f245c47cc452de0
Cristopher Castro <cristophernic@live.cl>
2018-08-14 18:54:26 -0300

    .

--- application/view/examen/express.php
+++ application/view/examen/express.php
@@ -8,13 +8,13 @@
                         <h6 class="text-center text-secondary">Paso 1</h6>
                         <h1 class="text-center mb-4">¿Qué desea realizar?</h1>
                         <div class="row justify-content-md-center">
-                            <div class="col-3 border border-primary rounded">
+                            <div class="col-3 border border-primary rounded mr-1">
                                 <h3 class="card-title text-primary">Exámen paciente virtual</h3>
-                                <button type="button" class="btn btn btn-outline-secondary mb-4" id="step.examen"><i class="fas fa-male"></i> Ir</button>
+                                <button type="button" class="btn btn btn-outline-secondary mb-4" id="step.examen"><i class="fas fa-male"></i> Ir al exámen</button>
                             </div>
-                            <div class="col-3 border border-primary rounded">
+                            <div class="col-3 border border-primary rounded mr-1">
                                 <h3 class="card-title text-primary">Ingresar nuevo paciente</h3>
-                                <button type="button" class="btn btn btn-outline-secondary mb-4" id="step.new"><i class="fas fa-user-circle my-2"></i> Ir</button>
+                                <button type="button" class="btn btn btn-outline-secondary mb-4" id="step.new"><i class="fas fa-user-circle my-2"></i> Ir al exámen</button>
                             </div>
                             <div class="col-4 border border-primary rounded">
                                 <h3 class="card-title text-primary">Buscar paciente</h3>
