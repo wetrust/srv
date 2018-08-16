fb7c5f1ed50e6580f87e9178702e8659b3c91aaa
Cristopher Castro <cristophernic@live.cl>
2018-08-16 18:08:34 -0300

    .

--- application/model/DicomModel.php
+++ application/model/DicomModel.php
@@ -114,7 +114,7 @@ class DicomModel
         //cambiar al directorio temporal
         $out = chdir ($folder . "tmp");
         //extraer todos los frames del archivo 
-        $strCommand =  "/usr/bin/dcml2pnm +Fa +oj +Jq 100". $folder . $file . " \ frame ";
+        $strCommand =  "/usr/bin/dcml2pnm +Fa +obr". $folder . $file . " \ frame ";
         $out = exec($strCommand);
     
    //     $archivos = scandir($folder . "/tmp");
