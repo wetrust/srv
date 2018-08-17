09cbc794bc47fb804d7174f7573cdc960398c9c2
Cristopher Castro <cristophernic@live.cl>
2018-08-16 21:20:00 -0300

    .

--- application/view/examen/express.php
+++ application/view/examen/express.php
@@ -1940,8 +1940,8 @@
             <div class="card position-fixed" id="ecoDopplerMenu">
                 <div class="card-body">
                     <h6 class="text-center my-2">Acciones</h6>
-                    <div class="btn-group-vertical d-flex justify-content-center" role="group" aria-label="First group">
-                    <button type="button" class="btn btn-secondary" id="boton.eco.doppler.nuevo">Nuevo</button><button type="button" class="btn btn-secondary" id="boton.eco.doppler.guardar">Guardar</button><button type="button" class="btn btn-secondary d-none">Cancelar</button><button type="button" class="btn btn-secondary" id="boton.eco.doppler.eliminar">Eliminar</button>
+                    <div class="btn-group-vertical d-flex" role="group" aria-label="First group">
+                        <button type="button" class="btn btn-secondary" id="boton.eco.doppler.nuevo">Nuevo</button><button type="button" class="btn btn-secondary" id="boton.eco.doppler.guardar">Guardar</button><button type="button" class="btn btn-secondary d-none">Cancelar</button><button type="button" class="btn btn-secondary" id="boton.eco.doppler.eliminar">Eliminar</button>
                     </div>
                     <hr>
                     <h6 class="text-center my-2">Informes</h6>
