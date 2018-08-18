658406781804c9a36eff92ff0cdd901e6ce0c254
Cristopher Castro <cristophernic@live.cl>
2018-08-18 15:06:31 -0300

    .

--- application/view/examen/express.php
+++ application/view/examen/express.php
@@ -789,7 +789,7 @@
             <div class="card mt-3">
                 <div class="card-body">
                     <div class="row justify-content-md-center">
-                        <div class="col-8">
+                        <div class="col-10">
                             <p>Ver imágenes de exámenes ultrasonográficos almacenados en base de datos <a href="#imgDicom" role="button" class="btn">Ver Imágenes ecográficas</a></p>
                         </div>
                     </div>
