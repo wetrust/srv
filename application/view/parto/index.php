bc6fe99a9a0e0270427486502939fc2f5cb01f98
Cristopher Castro <cristophernic@live.cl>
2018-08-27 15:37:53 -0300

    .

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -23,15 +23,12 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ingresar</button>
+            </form>
         </div>
     </nav>
         <section>
