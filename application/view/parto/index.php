63a9e8974354e83b507a08b5d91ea11e8f146e48
Cristopher Castro <cristophernic@live.cl>
2018-08-27 15:32:42 -0300

    c

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -15,7 +15,7 @@
 </head>
 <body class="h-100 w-100">
   <div class="h-100 w-100" id="main">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <a class="navbar-brand" href="#">crecimientofetal.cl</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
