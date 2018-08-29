479886bf86516f3d70195655371b29482117a5e0
Cristopher Castro <cristophernic@live.cl>
2018-08-29 16:01:27 -0300

    .

--- public/js/parto.js
+++ public/js/parto.js
@@ -265,7 +265,7 @@ $(document).ready(function() {
         $("#boton\\.parto\\.guardar").removeClass("d-none");
         $("#boton\\.parto\\.nuevo").addClass("d-none");
         $("#boton\\.parto\\.modificar").addClass("d-none");
-        $("#boton\\.parto\\.eliminar").removeClass("d-none");
+        $("#boton\\.parto\\.eliminar").addClass("d-none");
         //desbloquear cajas
         $("#id_paciente").prop("disabled", false).val("");
         $("#nombre_madre").prop("disabled", false).val("");
@@ -464,6 +464,49 @@ $(document).ready(function() {
 
     $("#boton\\.parto\\.eliminar").on("click", function() {
         var filas = $("#table\\.ecografia\\.parto").children();
+
+        $("#id_paciente").val("");
+        $("#nombre_madre").val("");
+        $("#apellido_madre").val("");
+        $("#lugar_parto_rn").val("");
+        $("#id_rn").val("");
+        $("#nombre_rn").val("");
+        $("#apellido_rn").val("");
+        $("#sexo_rn").val("");
+        $("#fecha_parto_rn").val("");
+        $("#datos\\.neonatal\\.edad").val("");
+        $("#termino_parto").val("");
+        $("#tipo_parto").val("");
+        $("#tipo_patologia_obstetrica").val("");
+        $("#meconio").val("");
+        $("#datos\\.neonatal\\.peso").val("");
+        $("#datos\\.neonatal\\.talla").val("");
+        $("#perimetro_craneo_rn").val("");
+        $("#datos\\.neonatal\\.ipn").val("");
+        $("#peso_placentario").val("");
+        $("#apgar_1").val("");
+        $("#apgar_5").val("");
+        $("#hiperbilirrubinemia").val("");
+        $("#poliglobulia").val("");
+        $("#hospital_ucin").val("");
+        $("#sindrome_respiratorio").val("");
+        $("#alta_con_madre").val("");
+        $("#observaciones").val("");
+        $("#prof\\.atencion\\.parto").val("");
+        $("#dextro_uno").val("");
+        $("#glicemia_uno").val("");
+        $("#conducta\\.uno").val(3);
+        $("#dextro_dos").val("");
+        $("#glicemia_dos").val("");
+        $("#conducta\\.dos").val(3);
+        $("#dextro_tres").val("");
+        $("#glicemia_tres").val("");
+        $("#conducta\\.tres").val(3);
+        $("#hipoglicemia_riesgo").val("");
+        $("#hipoglicemia_sospechada").val("");
+        $("#hipoglicemia_confirmada").val("");
+        $("#prof\\.alta\\.rn").val("");
+        $("#prof\\.atencion\\.parto").val("");
 
         $.each(filas, function(i, val) {
             if ($(val).hasClass('table-active') == true) {
