c9d650913e472478437acb6770494e8c6d14bf62
Cristopher Castro <cristophernic@live.cl>
2018-08-29 21:54:07 -0300

    .

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -502,7 +502,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="card mt-3">
+                        <div class="card my-3">
                             <div class="card-body">
                                 <p class="text-center text-primary">Cuatro ejemplos para ajuste al peso esperado según categoria de variables</p>
                                 <div class="btn-group d-flex justify-content-center" role="group" aria-label="Default button group">
