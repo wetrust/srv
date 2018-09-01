e0e588322e9d3ff52967dcf729a6d53bb9ea9e30
Cristopher Castro <cristophernic@live.cl>
2018-09-01 12:09:19 -0300

    .

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -534,7 +534,7 @@
                     <div class="col-12 h-100" style="overflow: auto;">
                         <div class="card">
                             <div class="card-header bg-primary">
-                                <h6 class="text-white">-  Hipoglicemia neonatal, como patología prevalente en el recien nacido, instrumento para validación clínica de nueva tabla de peso categorizada.</h6>
+                                <p class="text-white">-  Hipoglicemia neonatal, como patología prevalente en el recien nacido, instrumento para validación clínica de nueva tabla de peso categorizada.</p>
                             </div>
                             <div class="card-body">
                                 <div class="row">
@@ -630,7 +630,7 @@
                         </div>
                         <div class="card mt-2">
                             <div class="card-header">
-                                <h6>- Categorización del peso en funcion de la edad gestacional al parto: (1) Curva local generalizada y (2) curva general personalizada según variables.</h6>
+                                <p>- Categorización del peso en funcion de la edad gestacional al parto: (1) Curva local generalizada y (2) curva general personalizada según variables.</p>
                             </div>
                             <div class="card-body">
                                 <div class="row">
