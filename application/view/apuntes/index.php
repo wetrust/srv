e2961e300a7b2e870d85bbaa3e2f23ce310fb579
Cristopher Castro <cristophernic@live.cl>
2018-09-28 23:11:13 -0300

    .

--- application/view/apuntes/index.php
+++ application/view/apuntes/index.php
@@ -1,10 +1,67 @@
 <div class="container">
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    <ul class="nav mt-3">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Nuevo Apunte</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Configuración</a>
+        </li>
+    </ul>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Buscar</h5>
+            <form>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">por</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>actividades</option>
+                        <option>cancelacion</option>
+                        <option>lugar</option>
+                        <option>palabras claves</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">palabra</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1">
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Ultimos Apuntes</h5>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Fecha</th>
+                            <th scope="col">Resumen</th>
+                            <th scope="col">Palabras claves</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
