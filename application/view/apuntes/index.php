b694eb8a7c90eaeb6bba28084f46b12674113926
Cristopher Castro <cristophernic@live.cl>
2018-10-02 20:37:53 -0300

    .

--- application/view/apuntes/index.php
+++ application/view/apuntes/index.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="es" class="h-100">
    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
-      <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
-      <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/datepicker.css">
-      <title>Apuntes</title>
+        <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/datepicker.css">
+        <title>Apuntes</title>
    </head>
    <body class="h-100">
         <div class="row h-100 w-100" style="overflow:hide">
@@ -34,23 +34,23 @@
                     <div class="card-body">
                         <div class="form-group col"><label for="formulario.paciente">Nombre del evento o apuntes</label><input class="form-control" id="formulario.paciente" type="email"></div>
                         <div class="row m-0">
-                            <div class="form-group col"><label for="formulario.fecha">Fecha</label><input class="form-control" id="formulario.fecha" type="text"></div>
+                            <div class="form-group col"><label for="formulario.fecha">Fecha</label><input class="form-control" id="formulario.fecha" data-date-format="dd/mm/yyyy" type="text"></div>
                             <div class="form-group col"><label for="formulario.hora">Hora</label><input class="form-control" id="formulario.hora" type="text"></div>
                         </div>
                         <div class="row m-0">
-                            <div class="form-group col"><label for="formulario.actividad">Actividad realizada</label><select class="form-control" id="formulario.actividad"></select></div>
                             <div class="form-group col"><label for="formulario.lugar">Lugar del evento</label><select class="form-control" id="formulario.lugar"></select></div>
+                            <div class="form-group col"><label for="formulario.actividad">Búsqueda por actividad</label><select class="form-control" id="formulario.actividad"></select></div>
+                        </div>
+                        <div class="row m-0">
+                            <div class="form-group col"><label for="formulario.palabras">Asistentes al eventos</label><input class="form-control" id="formulario.palabras" type="text"></div>
+                            <input id="formulario.id" type="hidden" value="" />
                         </div>
                         <div class="row m-0">
                             <div class="form-group col"><label for="formulario.comentarios">Comentarios generales</label><textarea class="form-control" id="formulario.comentarios" rows="3"></textarea></div>
                         </div>
                         <div class="row m-0">
-                            <div class="form-group col"><label for="formulario.palabras">Palabras claves de búsqueda</label><input class="form-control" id="formulario.palabras" type="text"></div>
-                            <input id="formulario.id" type="hidden" value="" />
-                        </div>
-                        <div class="row m-0">
                             <div class="form-group col"><label for="formulario.cancelacion">Cancelación $</label><select class="form-control" id="formulario.cancelacion"></select></div>
-                            <div class="form-group col"><label for="formulario.fcancelacion">Fecha de cancelación</label><input class="form-control m-2" id="formulario.fcancelacion" type="text"></div>
+                            <div class="form-group col"><label for="formulario.fcancelacion">Fecha de cancelación</label><input class="form-control m-2" id="formulario.fcancelacion" type="text" data-date-format="dd/mm/yyyy"></div>
                             <div class="form-group col"><label for="formulario.valor">Valor cancelado</label><input class="form-control m-2" id="formulario.valor" type="number"></div>
                         </div>
                     </div>
@@ -67,7 +67,61 @@
                   </button>
                </div>
                <div class="modal-body" id="dialog.body">
-                  <p>Modal body text goes here.</p>
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item"> <a class="nav-link" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="false">Búsqueda por actividades</a> </li>
+                        <li class="nav-item"> <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Lugar de evento</a> </li>
+                        <li class="nav-item"> <a class="nav-link active show" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="true">Cancelacion</a> </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="btn-group" role="group" aria-label="Menú">
+                                <button type="button" class="btn btn-outline-success my-2 my-sm-0 mr-1" id="boton.actividad.nuevo" title="Nueva actividad"><i class="fas fa-pen"></i></button>
+                            </div>
+                            <div class="row d-none">
+                                <div class="form-group col"> <label for="exampleInputEmail1">Nombre de la actividad</label> <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"> </div>
+                                <div class="col-3"> <button class="btn btn-primary"><i class="fas fa-save"></i></button> <button class="btn btn-primary"><i class="fas fa-ban"></i></button> </div>
+                            </div>
+                            <table class="table table-hover">
+                                <thead class="table-success">
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nombre de la actividad</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tabla.actividad"> </tbody>
+                            </table>
+                        </div>
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="row">
+                                <div class="form-group col"> <label for="exampleInputEmail1">Nombre del Lugar</label> <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"> </div>
+                                <div class="col"> <button type="submit" class="btn btn-primary">Guardar</button> <button type="submit" class="btn btn-primary">Cancelar</button> </div>
+                            </div>
+                            <table class="table table-hover">
+                                <thead class="table-success">
+                                    <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nombre del Lugar</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tabla.actividad"> </tbody>
+                            </table>
+                        </div>
+                        <div class="tab-pane fade active show" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                            <div class="row">
+                                <div class="form-group col"> <label for="exampleInputEmail1">Tipo de cancelación</label> <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"> </div>
+                                <div class="col"> <button type="submit" class="btn btn-primary">Guardar</button><button type="submit" class="btn btn-primary">Cancelar</button></div>
+                            </div>
+                            <table class="table table-hover">
+                                <thead class="table-success">
+                                    <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Tipo de cancelación</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tabla.actividad"></tbody>
+                            </table>
+                        </div>
+                    </div>
                </div>
                <div class="modal-footer">
                   <button type="button" class="btn btn-primary" id="dialog.ok">Guardar</button>
