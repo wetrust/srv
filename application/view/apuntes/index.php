31143830d4732e8d3bf84451bdf0317d1d42c0a0
Cristopher Castro <cristophernic@live.cl>
2018-10-02 15:53:57 -0300

    .

--- application/view/apuntes/index.php
+++ application/view/apuntes/index.php
@@ -22,7 +22,7 @@
                     </div>
                     <form class="form-inline">
                         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
                     </form>
                 </nav>
                 <div style="overflow-y:scroll;height:calc(100% - 4.8rem);">
