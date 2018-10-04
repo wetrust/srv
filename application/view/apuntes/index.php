671c62441bd4879e0b8801f397dcaa103eba95f4
Cristopher Castro <cristophernic@live.cl>
2018-10-04 17:54:26 -0300

    .

--- application/view/apuntes/index.php
+++ application/view/apuntes/index.php
@@ -95,7 +95,7 @@
                 $("#dialog\\.title").html("Calculos");
                 $("#dialog\\.delete").remove();
                 $("#dialog\\.view").modal("show");
-                $("#dialog\\.body").html('<div class="row"><div class="col"><div class="form-group"><label for="calculos.fecha.uno">Fecha inicio</label><input type="text" class="form-control" id="calculos.fecha.uno"></div></div><div class="col"><div class="form-group"><label for="calculos.fecha.uno">Fecha fin</label><input type="text" class="form-control" id="calculos.fecha.dos"></div></div></div>');
+                $("#dialog\\.body").html('<div class="row"><div class="col"><div class="form-group"><label for="calculos.fecha.uno">Fecha inicio</label><input type="text" class="form-control" id="calculos.fecha.uno"></div></div><div class="col"><div class="form-group"><label for="calculos.fecha.uno">Fecha fin</label><input type="text" class="form-control" id="calculos.fecha.dos"></div></div></div><div class="row"><div class="col"><div class="table-responsive"><table class="table"></table></div></div></div>');
             });
             $("#boton\\.buscar").on("click", function(){
                 if ($("#div\\.busqueda").hasClass("d-none")){
