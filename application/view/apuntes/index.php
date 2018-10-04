342da362ea8ed72cdb20ac0c27aac84435b9c552
Cristopher Castro <cristophernic@live.cl>
2018-10-03 21:06:32 -0300

    .

--- application/view/apuntes/index.php
+++ application/view/apuntes/index.php
@@ -37,8 +37,8 @@
                             <div class="col">
                                 <div class="form-group"><label for="formulario.fecha"><strong>Fecha</strong></label><input class="form-control" id="formulario.fecha" data-date-format="dd/mm/yyyy" type="text" disabled></div>
                                 <div class="form-group"><label for="formulario.hora"><strong>Hora</strong></label><input class="form-control" id="formulario.hora" type="text" disabled></div>
-                                <div class="form-group"><label for="formulario.lugar"><strong>Lugar del evento</strong>(<i class="fas fa-search text-success"></i>)</label><select class="form-control" id="formulario.lugar" disabled></select></div>
-                                <div class="form-group"><label for="formulario.actividad"><strong>Método de búsqueda</strong>(<i class="fas fa-search text-success"></i>)</label><select class="form-control" id="formulario.actividad" disabled></select></div>
+                                <div class="form-group"><label for="formulario.lugar"><strong>Lugar del evento</strong>(<i class="fas fa-cog text-success"></i>)</label><select class="form-control" id="formulario.lugar" disabled></select></div>
+                                <div class="form-group"><label for="formulario.actividad"><strong>Método de búsqueda</strong>(<i class="fas fa-cog text-success"></i>)</label><select class="form-control" id="formulario.actividad" disabled></select></div>
                                 <div class="form-group"><label for="formulario.palabras"><strong>Asistentes al eventos</strong></label><input class="form-control" id="formulario.palabras" type="text" disabled></div>
                                 <input id="formulario.id" type="hidden" value="" />
                                 <div class="form-check">
@@ -46,7 +46,7 @@
                                     <label class="form-check-label" for="opcion.financiera"><strong>Registro financiero</strong></label>
                                 </div>
                                 <div class="d-none" id="div.financiero">
-                                    <div class="form-group mt-4"><label for="formulario.cancelacion"><strong>Cancelación $</strong> (<i class="fas fa-search text-success"></i>)</label><select class="form-control" id="formulario.cancelacion" disabled></select></div>
+                                    <div class="form-group mt-4"><label for="formulario.cancelacion"><strong>Cancelación $</strong> (<i class="fas fa-cog text-success"></i>)</label><select class="form-control" id="formulario.cancelacion" disabled></select></div>
                                     <div class="form-group"><label for="formulario.fcancelacion"><strong>Fecha de cancelación</strong></label><input class="form-control" id="formulario.fcancelacion" type="text" data-date-format="dd/mm/yyyy" disabled></div>
                                     <div class="form-group"><label for="formulario.valor"><strong>Valor cancelado</strong></label><input class="form-control" id="formulario.valor" type="number" disabled></div>
                                 </div>
