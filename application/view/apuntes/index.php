3fa37a728f0c562829360579155e6cebd3364db2
Cristopher Castro <cristophernic@live.cl>
2018-10-04 16:33:14 -0300

    .

--- application/view/apuntes/index.php
+++ application/view/apuntes/index.php
@@ -115,7 +115,7 @@
                     accion: "nuevo",
                     fecha: day + "/" + month + "/" + dateTime.getFullYear(),
                     hora: "",
-                    paciente: "Nuevo apunte",
+                    paciente: "Nuevo evento",
                     actividad: "",
                     lugar: "",
                     cancelacion: "",
