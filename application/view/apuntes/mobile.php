3d7750baba273b51e4f1dbda903bee7bdc8d6ed0
Cristopher Castro <cristophernic@live.cl>
2018-10-10 16:19:27 -0300

    .

--- application/view/apuntes/mobile.php
+++ application/view/apuntes/mobile.php
@@ -28,16 +28,25 @@
                         <a class="nav-link" href="#">Pricing</a>
                     </li>
                 </ul>
-                <span class="navbar-text">
-                    Navbar text with an inline element
-                </span>
+                <div class="dropdown pull-xs-right">
+  <button class="btn bmd-btn-icon dropdown-toggle" type="button" id="lr1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <i class="material-icons">more_vert</i>
+  <div class="ripple-container"></div></button>
+  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="lr1" x-placement="bottom-end" style="position: absolute; top: 32px; left: 32px; will-change: top, left;">
+    <button class="dropdown-item" type="button">Action</button>
+    <button class="dropdown-item" type="button">Another action</button>
+    <button class="dropdown-item disabled" type="button">Disabled action</button>
+    <div class="dropdown-divider"> </div>
+    <a class="dropdown-item" href="#">Separated link</a>
+  </div>
+</div>
             </div>
         </nav>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
         <script src="<?php echo Config::get('URL'); ?>js/static/bootstrap-datepicker.js"></script>
-        //<script src="<?php echo Config::get('URL'); ?>js/apuntes.js"></script>
-        //<script>$(document).ready(function() { var dateTime = new Date(); var day = ("0" + dateTime.getDate()).slice(-2); var month = ("0" + (dateTime.getMonth() + 1)).slice(-2); $("#formulario\\.fecha").val((day) + "/" + (month) + "/" + dateTime.getFullYear()); $('#formulario\\.fecha').datepicker('setValue', (day) + "/" + (month) + "/" + dateTime.getFullYear());$("#formulario\\.fcancelacion").val((day) + "/" + (month) + "/" + dateTime.getFullYear()); $('#formulario\\.fcancelacion').datepicker('setValue', (day) + "/" + (month) + "/" + dateTime.getFullYear());});</script>
+        <!--<script src="<?php echo Config::get('URL'); ?>js/apuntes.js"></script>
+        <script>$(document).ready(function() { var dateTime = new Date(); var day = ("0" + dateTime.getDate()).slice(-2); var month = ("0" + (dateTime.getMonth() + 1)).slice(-2); $("#formulario\\.fecha").val((day) + "/" + (month) + "/" + dateTime.getFullYear()); $('#formulario\\.fecha').datepicker('setValue', (day) + "/" + (month) + "/" + dateTime.getFullYear());$("#formulario\\.fcancelacion").val((day) + "/" + (month) + "/" + dateTime.getFullYear()); $('#formulario\\.fcancelacion').datepicker('setValue', (day) + "/" + (month) + "/" + dateTime.getFullYear());});</script> -->
     </body>
 </html>
