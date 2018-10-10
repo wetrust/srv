d2e85ef119dce4e9a476c3c69bc44ce94cefd43a
Cristopher Castro <cristophernic@live.cl>
2018-10-10 16:59:37 -0300

    .

--- application/view/apuntes/mobile.php
+++ application/view/apuntes/mobile.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="es" class="bg-secondary">
+<html lang="es" class="bg-secondary h-100">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
