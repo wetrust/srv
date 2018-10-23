47a4327e0870d6c6197ae3f266e3ff096ef571d8
Cristopher Castro <cristophernic@live.cl>
2018-10-23 18:41:35 -0300

    .

--- application/view/login/apuntes.php
+++ application/view/login/apuntes.php
@@ -89,7 +89,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
-            $("#boton\\.registrarse").on("click", function(){
+            $("#boton\\.registrarse").on("click", function(e){
+                e.preventDefault();
                 $("#registro\\.apuntes").removeClass("d-none");
             });
         });
