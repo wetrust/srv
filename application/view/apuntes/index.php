443e78286685dddf6579d8c9fa70e7af14098114
Cristopher Castro <cristophernic@live.cl>
2018-10-25 21:54:36 -0300

    .

--- application/view/apuntes/index.php
+++ application/view/apuntes/index.php
@@ -58,11 +58,11 @@
                                 <div class="form-group"><label for="formulario.lugar"><strong>Lugar del evento</strong>(<i class="fas fa-cog text-success"></i>)</label><select class="form-control" id="formulario.lugar" disabled></select></div>
                                 <div class="form-group"><label for="formulario.fecha"><strong>Fecha</strong></label><input class="form-control" id="formulario.fecha" data-date-format="dd/mm/yyyy" type="text" disabled></div>
                                 <div class="row">
-                                    <div class="form-group col-6">
+                                    <div class="form-group col-3">
                                         <label for="formulario.hora"><strong>Hora</strong></label>
                                         <select class="form-control" id="formulario.hora" disabled=""></select>
                                     </div>
-                                    <div class="form-group col-6">
+                                    <div class="form-group col-3">
                                         <label for="formulario.hora"><strong>Minutos</strong></label>
                                         <select class="form-control" id="formulario.minutos" disabled=""></select>
                                     </div>
