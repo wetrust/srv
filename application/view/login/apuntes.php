6d6fb0a9a6361d47110adc2e2f4a10e59d0ead11
Cristopher Castro <cristophernic@live.cl>
2018-10-31 15:53:13 -0300

    .

--- application/view/login/apuntes.php
+++ application/view/login/apuntes.php
@@ -13,7 +13,7 @@
         <body>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div class="container">
-                    <a class="navbar-brand" href="<?php echo Config::get('URL'); ?>apuntes">Apuntes de notas (reuniones, eventos clínicos, etc.)</a>
+                    <a class="navbar-brand" href="<?php echo Config::get('URL'); ?>apuntes">Apuntes de Notas (reuniones, eventos clínicos, etc.)</a>
                 </div>
             </nav>
             <div class="container mt-3">
