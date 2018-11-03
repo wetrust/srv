d6d59c20d53eb0e08474c5a1f8276225146863d2
Cristopher Castro <cristophernic@live.cl>
2018-11-03 17:14:31 -0300

    .

--- public/js/apuntes.js
+++ public/js/apuntes.js
@@ -367,6 +367,7 @@ $(document).ready(function(){
         $.post("https://servidor.crecimientofetal.cl/apuntes/api", formulario).done(function(data){
             if (Object.keys(data).length > 0) {
                 $('#actividad\\.preset').prop("checked", parseInt(data.user_actividad) === 1 ? true : false);
+                $('#actividad\\.preset').text(parseInt(data.user_actividad) === 1 ? Si : No);
                 $('#actividad\\.preset').on("click", function(){
                     $("#dialog\\.delete").remove();
                     $("#dialog\\.title").html('Configuración predeterminada para Tipo de evento');
@@ -374,7 +375,7 @@ $(document).ready(function(){
                         $("#dialog\\.body").html('<div class="row"> <div class="col-8"> <p class="text-center">¿Está seguro de eliminar la configuración predeterminada para tipo de evento?</p></div><div class="col-4"><button type="button" class="btn btn-primary" id="dialog.cancel">NO</button><button type="button" class="btn btn-danger" id="dialog.delete" data-id="0">SI</button> </div></div>');
                     }
                     else{
-                        $("#dialog\\.body").html('<div class="row"> <div class="col-8"> <p class="text-center">¿Desea añadir realmente la configuración predeterminada para tipo de evento?</p></div><div class="col-4"><button type="button" class="btn btn-primary" id="dialog.cancel">NO</button><button type="button" class="btn btn-danger" id="dialog.delete" data-id="1">SI</button> </div></div>');
+                        $("#dialog\\.body").html('<div class="row"> <div class="col-8"> <p class="text-center">¿Desea añadir configuración predeterminada para tipo de evento?</p></div><div class="col-4"><button type="button" class="btn btn-primary" id="dialog.cancel">NO</button><button type="button" class="btn btn-danger" id="dialog.delete" data-id="1">SI</button> </div></div>');
                     }
                     
                     $("#dialog\\.footer").children("button").addClass("d-none");
