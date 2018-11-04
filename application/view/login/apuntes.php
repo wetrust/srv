0c14e35a288166e2e5788c3c512852671f7c70c0
Cristopher Castro <cristophernic@live.cl>
2018-11-04 14:20:45 -0300

    .

--- application/view/login/apuntes.php
+++ application/view/login/apuntes.php
@@ -67,7 +67,7 @@
                                         <input type="email" class="form-control" name="user_email_repeat"/>
                                     </div>
                                     <div class="form-group">
-                                        <label>Contraseña</label>
+                                        <label>Contraseña (Mayor o igual a 6 carácteres)</label>
                                         <input type="password" class="form-control" name="user_password_new" pattern=".{6,}" />
                                     </div>
                                     <div class="form-group">
