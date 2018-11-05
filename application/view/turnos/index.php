7c9916fe032be2a9fcca8997af543022809a8576
Cristopher Castro <cristophernic@live.cl>
2018-11-05 16:22:16 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -135,6 +135,10 @@
                 </div>
             </div>
             <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                    </div>
+                </div>
             </div>
         </div>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
