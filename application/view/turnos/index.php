2db90087dfc696a12baa2bb5358bd57848a10dde
Cristopher Castro <cristophernic@live.cl>
2018-11-05 22:10:07 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -166,17 +166,17 @@
                     $("#div\\.profesional").addClass("d-none");
                     let datos = {
                         accion: "profesionalesNuevo",
-                        nombre: $("#profesional\\.nombre").val(""),
-                        rut: $("#profesional\\.rut").val(""),
-                        telefono: $("#profesional\\.telefono").val(""),
-                        correo: $("#profesional\\.correo").val("") 
+                        nombre: $("#profesional\\.nombre").val(),
+                        rut: $("#profesional\\.rut").val(),
+                        telefono: $("#profesional\\.telefono").val(),
+                        correo: $("#profesional\\.correo").val() 
                     }
 
                     $.post("https://servidor.crecimientofetal.cl/turnos/api", datos).done(function(response){
-                        $("#profesional\\.nombre").val();
-                        $("#profesional\\.rut").val();
-                        $("#profesional\\.telefono").val();
-                        $("#profesional\\.correo").val();
+                        $("#profesional\\.nombre").val("");
+                        $("#profesional\\.rut").val("");
+                        $("#profesional\\.telefono").val("");
+                        $("#profesional\\.correo").val("");
                         $("#boton\\.profesional\\.nuevo").removeClass("d-none");
                         $("#boton\\.profesional\\.guardar").addClass("d-none");
                         $("#boton\\.profesional\\.cancelar").addClass("d-none");
