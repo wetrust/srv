a8752a0290794a1c8043f78902064094f7a484c3
Cristopher Castro <cristophernic@live.cl>
2018-11-08 00:55:34 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -132,7 +132,7 @@
 
             $("#boton\\.turno").on("click", function(){
                 $("#dialog\\.title").html("Añadir datos (Profesional, fecha y horario de jornada)");
-                $("#dialog\\.body").html('<div class="row"><div class="form-group col"><label for="turnos.profesionales">Profesional</label><select class="form-control" id="turnos.profesionales"></select></div></div><div class="row"><div class="form-group col"><label for="turnos.fecha.in">Fecha</label><input class="form-control" type="date" id="turnos.fecha.in"></div><div class="form-group col"><label for="turnos.hora.in">Jornada</label><select class="form-control" id="turnos.turno"><option value="0">Día</option><option value="1">Noche</option></select></div></div>');
+                $("#dialog\\.body").html('<div class="row"><div class="form-group col"><label for="turnos.profesionales">Profesional</label><select class="form-control" id="turnos.profesionales"></select></div></div><div class="row"><div class="form-group col"><label for="turnos.fecha.in">Fecha</label><input class="form-control" type="date" id="turnos.fecha.in"></div><div class="form-group col"><label for="turnos.hora.in">Turno</label><select class="form-control" id="turnos.turno"><option value="0">Diurno</option><option value="1">Nocturno</option></select></div></div>');
                 $("#dialog\\.view").modal("show");
                 cargarProfesionales();
                 $("#dialog\\.delete").remove();
