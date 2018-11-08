d7b6485dde76ccabc5bc96a483101d7972f7bd07
Cristopher Castro <cristophernic@live.cl>
2018-11-08 16:00:48 -0300

    .

--- application/view/login/turnos.php
+++ application/view/login/turnos.php
@@ -52,13 +52,4 @@
             </div>
         </div>
     </body>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $("#boton\\.registrarse").on("click", function(e){
-                e.preventDefault();
-                $("#registro\\.apuntes").removeClass("d-none");
-            });
-        });
-    </script>
 </html>
