d4c9dc80d21c2a955b61df4f8776ecd5399201eb
Cristopher Castro <cristophernic@live.cl>
2018-11-10 17:31:08 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -126,7 +126,7 @@
             makeCalendario();
 
             $("#boton\\.turno").on("click", function(){
-                $("#dialog\\.title").html("Añadir datos (Fecha, horario y profesional de turno)");
+                $("#dialog\\.title").html("INGRESAR DATOS (fecha, horario y profesional de turno)");
                 $("#dialog\\.body").html('<div class="row"><div class="form-group col"><label for="turnos.fecha.in">Fecha de turno</label><input class="form-control" type="date" id="turnos.fecha.in"></div><div class="form-group col"><label for="turnos.hora.in">Tipo de turno</label><select class="form-control" id="turnos.turno"><option value="0">Diurno</option><option value="1">Nocturno</option><option value="2">Completo</option></select></div></div><div class="row"><div class="form-group col"><label for="turnos.profesionales">Profesional asignado</label><select class="form-control" id="turnos.profesionales"></select></div></div>');
                 $("#dialog\\.view").modal("show");
                 cargarProfesionales();
