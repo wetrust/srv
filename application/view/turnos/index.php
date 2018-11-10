75e05028439d4e155832c5e94e4deb9b35d88bf2
Cristopher Castro <cristophernic@live.cl>
2018-11-10 17:51:26 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -316,7 +316,7 @@
                                 let dateComplete = day + "-" + month + "-" + d.getFullYear();
 
                                 $("#dialog\\.title").html('CAMBIO PROFESIONAL DE TURNO:');
-                                $("#dialog\\.body").html('<div class="row"><div class="col">' + response.turno_profesional_nombre + '</div></div><div class="row"><div class="form-group col"><label for="turnos.profesionales" class="text-danger text-center mt-3"><strong>Reemplazar por Profesional:</strong></label><select class="form-control" id="turnos.profesionales"></select></div></div>');
+                                $("#dialog\\.body").html('<div class="row"><div class="col"><p>' + response.turno_profesional_nombre + ', fecha: ' + dateComplete +'</p></div></div><div class="row"><div class="form-group col"><label for="turnos.profesionales" class="text-danger text-center mt-3"><strong>Reemplazar por Profesional:</strong></label><select class="form-control" id="turnos.profesionales"></select></div></div>');
                                 $("#dialog\\.footer").append('<button type="button" class="btn btn-danger" id="dialog.delete" data-id="' + response.turno_id + '">Guardar</button>');
                                 cargarProfesionales();
 
