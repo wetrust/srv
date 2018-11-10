52f3be9e13f340eb6f56c8f73c5fe13a0014b54f
Cristopher Castro <cristophernic@live.cl>
2018-11-10 18:35:37 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -20,13 +20,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item"><a class="nav-link" href="#" id="boton.turno">Ingresar datos</a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Configuración</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#" id="boton.profesionales">Profesionales</a>
-                            </div>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="https://servidor.crecimientofetal.cl/login/logoutturnos">Salir</a></li>
+                        <li class="nav-item"><a class="dropdown-item" href="#" id="boton.profesionales">Configurar profesionales</a></li>
+                        <li class="nav-item"><a class="nav-link" href="https://servidor.crecimientofetal.cl/login/logoutturnos">Salir del programa</a></li>
                     </ul>
                 </div>
             </nav>
