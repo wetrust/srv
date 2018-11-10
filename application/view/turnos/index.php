38a4a7a64e8fbff98d1fff4a945947c62be12c49
Cristopher Castro <cristophernic@live.cl>
2018-11-10 18:31:46 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -19,7 +19,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item"><a class="nav-link" href="#" id="boton.turno">Nuevo Turno</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" id="boton.turno">Ingresar datos</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Configuración</a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
