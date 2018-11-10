a6bdf6384b7647af5309d09121a286d6ac5d7f74
Cristopher Castro <cristophernic@live.cl>
2018-11-10 18:05:56 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -11,27 +11,26 @@
         <title>Turnos</title>
     </head>
     <body class="h-100">
-        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Turno CAT</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link" href="#" id="boton.turno">Nuevo Turno</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Configuración</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#" id="boton.profesionales">Profesionales</a>
-                        </div>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="https://servidor.crecimientofetal.cl/login/logoutturnos">Salir</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class="row m-0">
-            <div class="col">
-                <div class="card ml-3 my-3">
+        <div class="fixed-top">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Turno CAT</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a class="nav-link" href="#" id="boton.turno">Nuevo Turno</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Configuración</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#" id="boton.profesionales">Profesionales</a>
+                            </div>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="https://servidor.crecimientofetal.cl/login/logoutturnos">Salir</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="card ml-3 my-3">
                     <div class="card-body">
                         <div class="form-row">
                             <div class="col"><p>Mes</p></div>
@@ -64,6 +63,8 @@
                         </div>
                     </div>
                 </div>
+        <div class="row m-0">
+            <div class="col">
                 <div class="card ml-3">
                     <div class="card-body">
                         <table class="table table-td table-hover table-bordered">
