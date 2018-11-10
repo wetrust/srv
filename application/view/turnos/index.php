31517bb0c240d16dd7a9cc485351e66130af3dbe
Cristopher Castro <cristophernic@live.cl>
2018-11-10 18:31:01 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -13,7 +13,7 @@
     <body class="h-100">
         <div class="fixed-top">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Turno CAT</a>
+                <a class="navbar-brand" href="#">CALENDARIO DE TURNOS</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
