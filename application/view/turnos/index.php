b4cb47c57874b40f5af93a23307559428f11ffcd
Cristopher Castro <cristophernic@live.cl>
2018-11-10 17:56:18 -0300

    .

--- application/view/turnos/index.php
+++ application/view/turnos/index.php
@@ -11,7 +11,7 @@
         <title>Turnos</title>
     </head>
     <body class="h-100">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Turno CAT</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
